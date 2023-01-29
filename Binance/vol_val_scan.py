d2b30f1a5e678f6f5ec43cba7841dea3d851fd9d
Jeon Won <j2onwon@gmail.com>
2023-01-29 21:13:44 +0900

    edit Binance/vol_val_scan.py: Add color message

--- Binance/vol_val_scan.py
+++ Binance/vol_val_scan.py
@@ -15,16 +15,23 @@ Binance/vol_val_scan.py
 * Usage: 설정 값 입력 후 `python3 vol_scan.py` 명령어 실행
 """
 
+# 상승(GREEN) 및 하락(RED) 색상 표시를 위한 클래스
+class Colors:
+  RED = '\033[31m'
+  GREEN = '\033[32m'
+  RESET = '\033[0m'
+
+# 대충 상수...
 load_dotenv()
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")      # 텔레그렘 봇 토큰
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")  # 텔레그램 봇 아이디
-TICKER = "BTC/USDT"    # 거래량을 탐지할 바이낸스 거래소 Ticker
-INTERVAL = "15m"       # 캔들 유형
-SLEEP_TIME = 1         # 탐지 간격(초)
-VOL_STANDARD = 100     # 거래량 기준치(탐지 간격(초) 동안 거래량이 얼마 이상 발생했을 때 알림을 줄 것인가?)
-IS_ALARMING = True     # 소리 알림 여부
-IS_TELEGRAMING = True  # 텔레그램 알림 여부
-IS_LOGGING = True      # 로그 기록 여부
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")      ## 텔레그렘 봇 토큰
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")  ## 텔레그램 봇 아이디
+TICKER = "BTC/USDT"    ## 거래량을 탐지할 바이낸스 거래소 Ticker
+INTERVAL = "15m"       ## 캔들 유형
+SLEEP_TIME = 1         ## 탐지 간격(초)
+VOL_STANDARD = 100     ## 거래량 기준치(거래량이 얼마 이상 발생했을 때 알림을 줄 것인가?)
+IS_ALARMING = True     ## 소리 알림 여부
+IS_TELEGRAMING = False ## 텔레그램 알림 여부
+IS_LOGGING = True      ## 로그 기록 여부
 
 # 로그 설정
 if(IS_LOGGING):
@@ -39,7 +46,7 @@ if(IS_LOGGING):
 # 초기화
 bot = telegram.Bot(TELEGRAM_TOKEN)
 binance = ccxt.binance()
-ohlcv = binance.fetch_ohlcv(TICKER, INTERVAL, limit=1) # 시가, 고가, 저자, 종가, 거래량(Open, High, Low, Close, Volume) 얻어오기
+ohlcv = binance.fetch_ohlcv(TICKER, INTERVAL, limit=1) ## 시가, 고가, 저자, 종가, 거래량(Open, High, Low, Close, Volume) 얻어오기
 price_old = ohlcv[0][4]
 vol_old = ohlcv[0][5]
 
@@ -47,36 +54,53 @@ print(f"Binance {TICKER} {INTERVAL} 캔들 거래량을 {SLEEP_TIME}초 간격�
 print(f"거래량이 {SLEEP_TIME}초간 {VOL_STANDARD}개 이상 발생하면 알림이 발생합니다.")
 
 while True:
-  # 현재 가격과 거래량 얻어오기
-  ohlcv = binance.fetch_ohlcv(TICKER, INTERVAL, limit=1)
-  price_new = ohlcv[0][4]
-  vol_new = ohlcv[0][5]
+  try: 
+    # 현재 가격과 거래량 얻어오기
+    ohlcv = binance.fetch_ohlcv(TICKER, INTERVAL, limit=1)
+    price_new = ohlcv[0][4]
+    vol_new = ohlcv[0][5]
 
-  # 가격 증감분 및 거래량 증가분 계산
-  price_diff = price_new - price_old
-  vol_diff = round(vol_new - vol_old, 2)
-  if(vol_diff < 0):
-    vol_diff = round(vol_new, 2)
+    # 가격 증감분 및 거래량 증가분 계산
+    price_diff = price_new - price_old
+    vol_diff = round(vol_new - vol_old, 2)
+    if(vol_diff < 0):
+      vol_diff = round(vol_new)
 
-  # 거래량 증가분이 기준치 이면 알림
-  if(vol_diff >= VOL_STANDARD):
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # 푸시 알림용 메시지 생성: 상승(GREEN) 및 하락(RED) 색상 표시하지 않음
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     price_per = round((ohlcv[0][4] - ohlcv[0][1]) / ohlcv[0][1] * 100, 2)
-    message = f"{current_time} {TICKER} 거래량 {SLEEP_TIME}초간 {vol_diff} 발생 ({price_per}%)"
+    push_msg = f"{current_time} {TICKER} {INTERVAL} {SLEEP_TIME}초간 발생한 거래량: {vol_diff} ({price_per}%)"
 
-    if(IS_ALARMING):
-      threading.Thread(target=playsound, args=("alarm.mp3",), daemon=True).start()
-    
-    if(IS_TELEGRAMING):
-      bot.sendMessage(TELEGRAM_CHAT_ID, text=message)
-    
-    if(IS_LOGGING):
-      logger.info(message)
+    # 콘솔 출력용 메시지 생성: 상승(GREEN) 및 < 하락(RED) 색상 표시
+    if(price_diff > 0):
+      console_msg = f"{current_time} {TICKER} {INTERVAL} {SLEEP_TIME}초간 발생한 거래량: {Colors.GREEN}{vol_diff} ({price_per}%){Colors.RESET}"
+    elif(price_diff < 0):
+      console_msg = f"{current_time} {TICKER} {INTERVAL} {SLEEP_TIME}초간 발생한 거래량: {Colors.RED}{vol_diff} ({price_per}%){Colors.RESET}"
     else:
-      print(message)
+      console_msg = f"{current_time} {TICKER} {INTERVAL} {SLEEP_TIME}초간 발생한 거래량: {vol_diff} ({price_per}%)"
 
-  # 현재 가격 및 거래량을 Old 변수에 대입
-  price_old = price_new
-  vol_old = vol_new
+    # 거래량 증가분이 기준치 이상이면 알림
+    if(vol_diff >= VOL_STANDARD):
+      push_msg = push_msg + " 🌟"
+      console_msg = console_msg + " 🌟"
+      if(IS_ALARMING):
+        threading.Thread(target=playsound, args=("alarm.mp3",), daemon=True).start()
+      if(IS_TELEGRAMING):
+        bot.sendMessage(TELEGRAM_CHAT_ID, text=push_msg)
+      if(IS_LOGGING):
+        logger.info(push_msg)
+    
+    # 콘솔 메시지 출력
+    print(console_msg)
 
-  time.sleep(SLEEP_TIME)
+    # 현재 가격 및 거래량을 Old 변수에 대입한 후 SLEEP_TIME초 간 대기
+    price_old = price_new
+    vol_old = vol_new
+    time.sleep(SLEEP_TIME)
+
+  # 오류 발생 처리
+  except Exception as e:
+    if(IS_LOGGING):
+      logger.critical(e)
+    else:
+      print(e)
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # crypto-bot-python-v2
 
 ## 개요
+
 가상화폐 포지션 진입 시점이 다가왔다고 예상되었을 때 알림을 보내기 위한 파이썬 코드입니다.
 
 **테스트 중인 코드이므로 정상적인 실행을 보장하지 않으며, 이 프로그램을 사용하여 발생하는 손해에 대한 책임은 사용자 본인에게 있습니다.**
@@ -12,7 +13,8 @@
 * python-dotenv: 환경변수(.env)를 사용하기 위한 라이브러리
 * playsound: 사운드 재생을 위한 라이브러리
 
-## 사용법
+## 사용법(Binance)
+
 우선 루트 경로에 `.env` 파일을 만듭니다. 이 파일에 텔레그램 API 키 값을 명시합니다. 텔레그램 API는 텔레그램 메시지를 전송할 때 사용됩니다.
 
 ```
@@ -20,7 +22,7 @@ TELEGRAM_TOKEN = "tElEgRaMtOkEn"  # 텔레그렘 봇 토큰
 TELEGRAM_CHAT_ID = 12345678990    # 텔레그램 봇 아이디
 ```
 
-### 바이낸스 가상화폐 차트 조사
+### 차트 조사: alarm.py
 
 `Binance/alarm.py` 파일을 열어 설정을 위한 상수 값을 입력합니다.
 
@@ -62,10 +64,29 @@ SLEEP_TIME = 1
 
 5분봉을 조사하려면 `python3 Binance/alert.py 5m` 명령어를 실행합니다. 5m 대신 1m, 3m, 15m, 30m, 1h, 4h, 12h를 입력할 수 있습니다.
 
-5분봉을 백그라운드로 실행하여 조사하려면 `nohup python3 Binance/alert.py 5m &` 명령어를 실행합니다.
+5분봉을 백그라운드로 실행하여 조사하려면 `nohup python3 Binance/alert.py 5m &` 명령어를 실행하여 백그라운드로 작동시킵니다.
 
 프로그램을 실행하면 logs 폴더에 프로그램 작동 및 알림 이력 로그를 기록합니다.
 
-### 업비트 가상화폐 차트 조사
+### 순간 거래량 탐지: vol_val_scan.py
+
+`Binance/vol_val_scan.py` 파일을 열어 설정을 위한 상수 값을 입력합니다. SLEEP_TIME 값이 1이면 1초 간격으로 거래량이 얼마나 발생했는지 계산합니다. VOL_STANDARD 값이 100이면 거래량이 100 이상 발생 시 알림을 보냅니다.
+
+```python
+TICKER = "BTC/USDT"    # 거래량을 탐지할 바이낸스 거래소 Ticker
+INTERVAL = "15m"       # 캔들 유형
+SLEEP_TIME = 1         # 탐지 간격(초)
+VOL_STANDARD = 100     # 거래량 기준치(거래량이 얼마 이상 발생했을 때 알림을 줄 것인가?)
+IS_ALARMING = True     # 소리 알림 여부
+IS_TELEGRAMING = True  # 텔레그램 알림 여부
+IS_LOGGING = True      # 로그 기록 여부
+```
+
+상수 값을 입력한 후 `nohup python3 Binance/vol_val_scan.py &` 명령어를 실행하여 백그라운드로 작동시킵니다.
+
+IS_LOGGING 값이 True이면 Binance 폴더에 logs 폴더를 만들어놔야 하며, logs 폴더에 거래량 알림 발생 로그를 기록합니다.
+
+
+## 업비트 가상화폐 차트 조사
 
 요즘 가상화폐 현물장은 단타 치기에 영 좋지 않기 때문에 나중에...
